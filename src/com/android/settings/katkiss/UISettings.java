32a8595211cc59cbf049acfd95e06a7b2e0b5de9
timduru <timduru@gmail.com>
2013-08-09 14:38:42 +0200

    Add UI Settings
    Change-Id: Ic5e54d9f3d03d8dea5095c63c2bb05751f2e4187

---
+++ src/com/android/settings/katkiss/UISettings.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.katkiss;
+import org.meerkats.katkiss.KKC;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceScreen;
+
+import android.provider.Settings;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.app.Dialog;
+
+import java.util.ArrayList;
+
+public class UISettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener, OnPreferenceClickListener {
+    private static final String TAG = "UISettings";
+
+    private static final String KEY_UI_MODE = "kk_ui_mode";
+    private ContentResolver mResolver; 
+
+    private ListPreference _uiMode;
+    private boolean _prevTabletUIMode;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mResolver = getActivity().getContentResolver();
+
+        addPreferencesFromResource(R.xml.kk_ui_settings);
+        _uiMode = (ListPreference) findPreference(KEY_UI_MODE);
+        refreshState();
+
+        _uiMode.setOnPreferenceChangeListener(this);
+
+    }
+
+
+    private void refreshState() {
+        int uiVal =  Settings.System.getInt(mResolver, KKC.S.SYSTEMUI_UI_MODE, KKC.S.SYSTEMUI_UI_MODE_SYSTEMBAR );
+        _prevTabletUIMode = (uiVal == KKC.S.SYSTEMUI_UI_MODE_SYSTEMBAR);
+
+        _uiMode.setDefaultValue(String.valueOf(uiVal));
+        _uiMode.setValue(String.valueOf(uiVal));
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        refreshState();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    public Dialog onCreateDialog(int dialogId) {
+        return null;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+	if(key == null) return true;
+
+        if (key.equals(KEY_UI_MODE)) {
+          int mode = Integer.parseInt((String) objValue);
+
+          boolean tabletUIMode = (mode == KKC.S.SYSTEMUI_UI_MODE_SYSTEMBAR);
+          Settings.System.putInt(getContentResolver(), KKC.S.SYSTEMUI_UI_MODE, mode);
+          sendIntentToWindowManager(KKC.I.CMD_BARTYPE_CHANGED, true);
+          _prevTabletUIMode = tabletUIMode;
+        }
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+/*        if (preference == mFontSizePref) {
+            if (Utils.hasMultipleUsers(getActivity())) {
+                showDialog(DLG_GLOBAL_CHANGE_WARNING);
+                return true;
+            } else {
+                mFontSizePref.click();
+            }
+        }
+*/
+        return false;
+    }
+
+    private void sendIntentToWindowManager(String cmd, boolean shouldRestartUI) {
+        Intent intent = new Intent()
+                .setAction(KKC.I.UI_CHANGED)
+                .putExtra(KKC.I.CMD,  cmd)
+                .putExtra(KKC.I.EXTRA_RESTART_SYSTEMUI, shouldRestartUI);
+        getActivity().sendBroadcastAsUser(intent, new UserHandle(UserHandle.USER_ALL));
+    }
+
+
+}

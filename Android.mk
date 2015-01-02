81c00e72f222382f0705e0a45a155f26f0d84db1
Jorge Martín <angel.arasthel@gmail.com>
2015-01-02 12:14:14 +0100

    Add customization for Quick Settings [2/2]
    Add a new section to customize Quick Settings.
    
    Modified DraggableGridView to arrange the items differently.
    
    Gave everything a "Material-ish" style.
    
    Inversion and Hotspot tiles can be rearranged now.
    
    Creadted a "Notification Drawer" category.
    
    frameworks/base: http://review.cyanogenmod.org/83417/
    
    Change-Id: I88f9f067e60e66d51583219c497aab39cb187fb4
    
    Conflicts:
    	Android.mk
    	res/values/cm_plurals.xml
    	res/values/cm_strings.xml
    	res/values/dimens.xml
    	res/xml/dashboard_categories.xml
    
    Fix bug on QS selection preference.
    
    DraggableGridView allows you to put a tile after the Add/Delete one, causing buggy behaviour.
    
    Change-Id: Id0c61b188ce72d3bf371fd906ecd47c41c7ae528
    
    Settings: Update QS_TILES to point at secure settings.
    
    Change-Id: I600701b0c9f71f6569743ac5ae18bf5c96fe5b0a
    
    Cleanup and fix QS tile implementation
    
    - Decouple DraggableGridView and QSTiles code by shifting around some
      logic
    - Fix column-for-position calculation
    - Improve layouts and preference ordering
    
    Change-Id: I7a06150f2602888b0f28fca93e0164e381272b67
    
    Conflicts:
    	res/xml/notification_drawer_settings.xml
    
    fix
    
    rename packages
    
    QS: misc fixes

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_JAVA_LIBRARIES := bouncycastle conscrypt telephony-common
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13 jsr305
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13 jsr305 android-support-v7-cardview
 
 LOCAL_MODULE_TAGS := optional
 
@@ -10,13 +10,18 @@ LOCAL_SRC_FILES := \
         $(call all-java-files-under, src) \
         src/com/android/settings/EventLogTags.logtags
 
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res frameworks/support/v7/cardview/res
 
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_AAPT_FLAGS := --extra-packages android.support.v7.cardview
+
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+
 
 include frameworks/opt/setupwizard/navigationbar/common.mk
 

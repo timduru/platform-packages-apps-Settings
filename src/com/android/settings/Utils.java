39a98ce084fcab821b03866303c1f14d530cfb53
Danesh Mondegarian <daneshm90@gmail.com>
2015-01-20 01:40:00 -0800

    Settings : Update for centralized quick settings
    Change-Id: If9cad4fd45e5bf7deb13aa1a5284ac5566e44a95
    
    Conflicts:
    	res/values/cm_colors.xml
    	src/com/android/settings/Utils.java
    	src/com/android/settings/katkiss/qs/QSTileHolder.java

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -41,6 +41,7 @@ import android.content.res.Resources.NotFoundException;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.LinkProperties;
@@ -88,6 +89,8 @@ import java.util.Locale;
 
 public final class Utils {
     private static final String TAG = "Settings";
+
+    public static final String SYSTEM_UI_PACKAGE_NAME = "com.android.systemui";
 
     /**
      * Set the preference's title to the matching activity's label.
@@ -964,4 +967,36 @@ public final class Utils {
         }
         return sb.toString();
     }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
+    }
+
+    public static Drawable getNamedDrawableFromSystemUI(Resources res, String name) {
+        if (res == null) {
+            return null;
+        }
+        int resId = res.getIdentifier(name, "drawable", SYSTEM_UI_PACKAGE_NAME);
+        if (resId > 0) {
+            Drawable d = res.getDrawable(resId);
+            return d;
+        }
+        return null;
+    }
+
 }
